6f3cd177fefbcf8f3ecb4d2d2bbd1fedbc3928a5
Sven Sonntag <sven.sonntag@solution-developer.de>
2017-07-14 13:20:10 +0200

    update readme.php

--- readme.php
+++ readme.php
@@ -6,6 +6,10 @@
     </head>
     <body>
         <h1>Dokumentation zum Seminarmanager</h1>
+        <p>
+            Der Seminarmanager entstand als Projektarbeit innerhalb meiner Weiterbildung zum Web-Developer 2016 innerhalb des Kurses zum Thema PHP & Frameworks.<br>
+            Als Framework wurde hierbei Doctrine benutzt. Für die Realisierung war eine Zeit von 2 Tagen vorgesehen.
+        </p>
         <h2>Das Datenmodell</h2>
         <figure>
             <img src="img/seminarmanager.svg" alt="Das Datenmodel" width="45%">
@@ -25,10 +29,11 @@
             <li>Nachfrage beim Löschen von Datensätzen</li>
             <li>Validierung von Eingaben (z. Bsp. bereits vorhandene Kategorien)</li>
             <li>Weitere Spezifizierung der Seminardarstellung (z. Bsp. keine Kurse aus der Vergangenheit)</li>
+            <li>Demodaten erzeugen keine Kurse in der Vergangenheit</li>
         </ul>
         <h2>Erfahrungen</h2>
         <ul>
-            <li>Die Erstellung der Datenbankstruktur über Doctrine ist eine tolle Funktion!</li>
+            <li>Die Erstellung der Datenbankstruktur über Annotations bzw. Doctrine ist eine tolle Funktion!</li>
             <li>Leider ist das Debugging per var_dump() in Doctrine kaum möglich da die Objekte sehr umfangreich sind.</li>
             <li>In einem Framework kommt man viel schneller zu einer arbeitsfähigen Lösung.</li>
         </ul>
